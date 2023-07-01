02e7c171a3c820d1f4420d987824f22b336b176b
Kieran Croft <croftkie@proton.me>
2023-07-01 15:30:04 +0100

    UI prototype for leaderboard and progress comps added

--- src/comps/HUD/settings-comps/Leaderboard.jsx
+++ src/comps/HUD/settings-comps/Leaderboard.jsx
@@ -1,13 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import { images } from "../../../utils/images";
 
 const Leaderboard = () => {
-    return ( 
-        <div className="leaderboard">
-            <div className="top-bar">
-                <h3>Leaderboard</h3>
+  return (
+    <div className="leaderboard">
+      <div className="top-bar">
+        <h3>Leaderboard</h3>
+      </div>
+      <div className="board">
+        {[1, 2, 3, 4, 5, 6].map((item) => {
+          return (
+            <div className="card">
+              <div className="number">
+                <h4>{item}</h4>
+              </div>
+              <h4>1,000,000</h4>
+              <p>Username</p>
             </div>
-        </div>
-     );
-}
- 
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
 export default Leaderboard;
--- src/comps/HUD/settings-comps/Progress.jsx
+++ src/comps/HUD/settings-comps/Progress.jsx
@@ -1,13 +1,31 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const Progress = () => {
-    return ( 
-        <div className="progress">
-            <div className="top-bar">
-                <h3>Progress</h3>
-            </div>
+  return (
+    <div className="progress">
+      <div className="top-bar">
+        <h3>Progress</h3>
+      </div>
+      <div className="progress-points">
+        <div className="one">
+          <div className="point"></div>
+          <p>The Waterfall</p>
         </div>
-     );
-}
- 
+        <div className="two">
+          <p>The Waterfall</p>
+          <div className="point"></div>
+        </div>
+        <div className="three">
+          <div className="point"></div>
+          <p>The Waterfall</p>
+        </div>
+        <div className="four">
+          <p>The Waterfall</p>
+          <div className="point"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default Progress;
